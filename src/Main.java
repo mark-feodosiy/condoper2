1a7089307d23bf7d4173663f865cb8abac6d2dc4
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-15 23:29:23 +0300

    Вторая домашка с условними операторами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println();
+    int clientOS = 1;
+    switch (clientOS) {
+        case 0:
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            break;
+        case 1:
+            System.out.println("Установите версию приложения для Android по ссылке");
+            break;
+        default:
+            System.out.println("Выберите О или 1");
+    }
+
+
+
     }
 }
