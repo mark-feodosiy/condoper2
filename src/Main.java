e93f526d22f3520e3352f6fbbd8f59a3244eb6ee
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-16 19:57:03 +0300

    Вторая домашка с условними операторами

--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,51 @@ public class Main {
                     System.out.println("Установите версию приложения для Android по ссылке");
                 }
             }
-                System.out.println();
-                {
-                    // Задача № 2
-                    int userOS = 1;
-                    int yearOS = 2014;
-                    if (userOS == 0 && yearOS < 2015) {
-                        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                    } else if (userOS == 0 && yearOS >= 2015) {
-                        System.out.println("Установите версию приложения для iOS по ссылке");
-                    } else if (userOS == 1 && yearOS < 2015) {
-                        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                    } else if (userOS == 1 && yearOS >= 2015) {
-                        System.out.println("Установите версию приложения для Android по ссылке");
-                    }
-
+            System.out.println();
+            {
+                // Задача № 2
+                int userOS = 1;
+                int yearOS = 2014;
+                if (userOS == 0 && yearOS < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else if (userOS == 0 && yearOS >= 2015) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else if (userOS == 1 && yearOS < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else if (userOS == 1 && yearOS >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
                 }
+            }
+            System.out.println();
+            {
+                // Задача № 3
+                int year = 2021;
+                if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                    System.out.println(year + " год является високосным");
+                } else {
+                    System.out.println(year + " год является невисокосным");
+                }
+            }
+            System.out.println();
+            {
+                // Задача № 4
+                int deliveryDistance = 150;
+                if (deliveryDistance <= 20) {
+                    int deliveryDays = 1;
+                    System.out.println("Потребуется количество дней - " + deliveryDays);
+                    if (deliveryDistance > 20 && deliveryDistance <= 60)
+                        deliveryDays++;
+                        System.out.println("Потребуется количество дней - " + deliveryDays++);
+                    if (deliveryDistance > 60 && deliveryDistance <= 100)
+                        deliveryDays++;
+                        System.out.println("Потребуется количество дней - " + deliveryDays++);
+                    } else {
+                        System.out.println("Доставка не осуществляется");
+                    }
+                }
+
             }
 
         }
     }
+
