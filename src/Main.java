e27079e47422d8d54e8dd7c50f92e8cd2df78106
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-16 18:41:36 +0300

    Вторая домашка с условними операторами

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         System.out.println();
-    int clientOS = 1;
-    switch (clientOS) {
-        case 0:
-            System.out.println("Установите версию приложения для iOS по ссылке");
-            break;
-        case 1:
-            System.out.println("Установите версию приложения для Android по ссылке");
-            break;
-        default:
-            System.out.println("Выберите О или 1");
-    }
+        {
+            // Задача № 1
+            int clientOS = 1;
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                    break;
+                case 1:
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                    break;
+                default:
+                    System.out.println("Выберите О или 1");
+            }
 
+        System.out.println();
+        {
+            // Задача № 2
+            int clientOS = 0;
+            int yearOS = 2016;
+            if ((clientOS = 0) && (yearOS < 2015)) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOS = 0 && yearOS >= 2015) {
+                   System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientOS = 1 && yearOS < 2015) {
+                   System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else if (clientOS = 1 && yearOS >= 2015) {
+                   System.out.println("Установите версию приложения для Android по ссылке");
+                   }
 
+            }
+        }
 
     }
 }
