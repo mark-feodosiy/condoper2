556d60363edb1006cb8361ac38c0a8048926f0b4
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-16 23:01:00 +0300

    Вторая домашка с условними операторами

--- src/Main.java
+++ src/Main.java
@@ -56,9 +56,41 @@ public class Main {
                                     }
                     }
                 }
+        System.out.println();
+              {
+                  // Задача № 5
+                 int monthNumber = 13;
+                 switch (monthNumber) {
+                     case 12:
+                     case 1:
+                     case 2:
+                         System.out.println("Зима");
+                         break;
+                     case 3:
+                     case 4:
+                     case 5:
+                         System.out.println("Весна");
+                         break;
+                     case 6:
+                     case 7:
+                     case 8:
+                         System.out.println("Лето");
+                         break;
+                     case 9:
+                     case 10:
+                     case 11:
+                         System.out.println("Осень");
+                         break;
+                     default:
+                         System.out.println("Такого месяца не существует");
 
-            }
+
+                 }
+
 
         }
+    }
+}
+
 
 
