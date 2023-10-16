50abb1e3fc2d3bea9a103fdf7177c6c8a8bbac76
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-16 18:58:00 +0300

    Вторая домашка с условними операторами

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,30 @@ public class Main {
         {
             // Задача № 1
             int clientOS = 1;
-            switch (clientOS) {
-                case 0:
-                    System.out.println("Установите версию приложения для iOS по ссылке");
-                    break;
-                case 1:
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                if (clientOS == 1) {
                     System.out.println("Установите версию приложения для Android по ссылке");
-                    break;
-                default:
-                    System.out.println("Выберите О или 1");
+                }
+            }
+                System.out.println();
+                {
+                    // Задача № 2
+                    int userOS = 1;
+                    int yearOS = 2014;
+                    if (userOS == 0 && yearOS < 2015) {
+                        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    } else if (userOS == 0 && yearOS >= 2015) {
+                        System.out.println("Установите версию приложения для iOS по ссылке");
+                    } else if (userOS == 1 && yearOS < 2015) {
+                        System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                    } else if (userOS == 1 && yearOS >= 2015) {
+                        System.out.println("Установите версию приложения для Android по ссылке");
+                    }
+
+                }
             }
 
-        System.out.println();
-        {
-            // Задача № 2
-            int clientOS = 0;
-            int yearOS = 2016;
-            if ((clientOS = 0) && (yearOS < 2015)) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else if (clientOS = 0 && yearOS >= 2015) {
-                   System.out.println("Установите версию приложения для iOS по ссылке");
-            } else if (clientOS = 1 && yearOS < 2015) {
-                   System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else if (clientOS = 1 && yearOS >= 2015) {
-                   System.out.println("Установите версию приложения для Android по ссылке");
-                   }
-
-            }
         }
-
     }
-}
